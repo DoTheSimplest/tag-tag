d03edc84b7fd3eb2d512fb72e7869369800d7a76
DoTheSimplest <249650346+DoTheSimplest@users.noreply.github.com>
2026-01-01 03:27:55 +0900

    test: fix test for type parameter of `Tag`

--- test/Tag.test.ts
+++ test/Tag.test.ts
@@ -3,9 +3,9 @@ import { Tag } from "../src";
 
 describe(Tag, () => {
 	it("type parameter", () => {
-		const video = Tag<HTMLVideoElement>("<video></video>", e => {
-            assert.equal(e.src, undefined);
-        })
+		const video = Tag<HTMLVideoElement>("<video></video>", (e) => {
+			assert.equal(e.src, "");
+		});
 		assert.equal(video.tagName, "VIDEO");
 	});
 });
