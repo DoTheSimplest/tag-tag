c79d373f98c69251929f39e6b805af54e03fcb50
DoTheSimplest <249650346+DoTheSimplest@users.noreply.github.com>
2026-01-02 12:04:20 +0900

    refactor: replace `IfFlow` call with `Switch`

--- src/flow/If.ts
+++ src/flow/If.ts
@@ -1,59 +1,15 @@
-import { nodeData } from "../data/data";
-import { getNextNodeSibling } from "../initializeChildBlock";
-import { type Signal, useEffect } from "../signal/Signal";
-import { ControlFlow } from "./ControlFlow";
+import { type Signal, useComputed } from "../signal/Signal";
+import type { ControlFlow } from "./ControlFlow";
+import { Switch } from "./Switch";
 
 export function If(
 	condition: Signal<boolean>,
 	show: () => Element,
 	showElse?: () => Element,
 ): ControlFlow {
-	return new IfFlow(condition, show, showElse);
-}
-export class IfFlow extends ControlFlow {
-	#condition: Signal<boolean>;
-	#createThen: () => Element;
-	#createElse?: () => Element;
-
-	constructor(
-		condition: Signal<boolean>,
-		create: () => Element,
-		createElse?: () => Element,
-	) {
-		super();
-		this.#condition = condition;
-		this.#createThen = create;
-		this.#createElse = createElse;
-	}
-
-	run(element: Element) {
-		let child: Element;
-		let elseChild: Element | undefined;
-
-		const update = () => {
-			const next = getNextNodeSibling(this);
-
-			if (this.#condition.get()) {
-				if (!child) child = this.#createThen();
-
-				// data
-				nodeData.resolveCallbacks(element, child);
-				// insert `child`
-				this.firstNode = child;
-				elseChild?.remove();
-				element.insertBefore(child, next);
-			} else {
-				if (!elseChild) elseChild = this.#createElse?.();
-
-				// data
-				elseChild && nodeData.resolveCallbacks(element, elseChild);
-				// insert `elseChild`
-				this.firstNode = elseChild ?? null;
-				child?.remove();
-				elseChild && element.insertBefore(elseChild, next);
-			}
-		};
-
-		useEffect(update);
-	}
+	return Switch(
+		useComputed(() => `${condition.get()}`),
+		{ true: show },
+		showElse,
+	);
 }
